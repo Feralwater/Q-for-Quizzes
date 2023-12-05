29d1b36090687bba00300ca419a5bd74ef3626c2
BuyakovaVA <lineviktoriya@gmail.com>
2023-12-05 20:28:48 +0100

    Install vuetify

---
+++ src/vuetify/index.js
@@ -0,0 +1,20 @@
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: "#27ff2b",
+        }
+      }
+    }
+  }
+});
+
+export default vuetify;
