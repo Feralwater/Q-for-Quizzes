05c884f98d11db4685c68dd95877f02cc3e6b02f
BuyakovaVA <lineviktoriya@gmail.com>
2023-12-06 14:33:56 +0100

    add countdown component

---
+++ src/components/CountDown/CountDown.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+
+defineProps({
+  time: {
+    type: Number,
+    default: 0,
+  }
+})
+</script>
+
+<template>
+  <div class="countdown">
+    <span class="countdown__time">
+      {{ time }}
+    </span>
+    <span>
+      sec
+    </span>
+  </div>
+</template>
+
+<style scoped>
+@import './CountDown.scss';
+</style>
