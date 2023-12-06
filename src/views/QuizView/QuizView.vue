1f92d725535b6b5927b304e03e973d89dc78d4d4
BuyakovaVA <lineviktoriya@gmail.com>
2023-12-06 21:34:00 +0100

    add quiz view

--- src/views/QuizView/QuizView.vue
+++ src/views/QuizView/QuizView.vue
@@ -1,13 +1,64 @@
 <script setup lang="ts">
-
+import thinkingMan from "@/assets/images/thinkingMan.png"
+import CountDown from "@/components/CountDown/CountDown.vue";
 </script>
 
 <template>
-  <div>
-    <h1>QuizView</h1>
+  <div class="quiz">
+    <div class="quiz__countdown">
+      <CountDown />
+    </div>
+    <section class="quiz__inner">
+      <div class="quiz__question">
+        question
+      </div>
+      <div class="quiz__answersContainer">
+        <fieldset class="quiz__answers">
+          <div class="quiz__answer">
+            <input
+              id="answer1"
+              type="radio"
+              name="answer"
+            >
+            <label for="answer1">answer1</label>
+          </div>
+          <div class="quiz__answer">
+            <input
+              id="answer2"
+              type="radio"
+              name="answer"
+            >
+            <label for="answer2">answer2</label>
+          </div>
+          <div class="quiz__answer">
+            <input
+              id="answer3"
+              type="radio"
+              name="answer"
+            >
+            <label for="answer3">answer3</label>
+          </div>
+          <div class="quiz__answer">
+            <input
+              id="answer4"
+              type="radio"
+              name="answer"
+            >
+            <label for="answer4">answer4</label>
+          </div>
+        </fieldset>
+        <div class="quiz__image">
+          <v-img
+            :src="thinkingMan"
+            width="300px"
+            height="300px"
+          />
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <style scoped>
-
+@import "./QuizView.scss";
 </style>
