81044e57b9d311fed01d67707df5a148e086df26
BuyakovaVA <lineviktoriya@gmail.com>
2023-12-06 20:23:01 +0100

    add Quiz View

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
-import CountDown from "@/components/CountDown/CountDown.vue";
 </script>
 
 <template>
-  <CountDown :time="10" />
-
   <RouterView />
 </template>
-
-<style scoped>
-
-</style>
