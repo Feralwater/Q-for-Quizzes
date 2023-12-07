0afd6693b4370bfd9d5a7d30ac640523f5fab9ad
BuyakovaVA <lineviktoriya@gmail.com>
2023-12-07 19:56:37 +0100

    remove watch method

--- src/components/CountDown/useCountDown.ts
+++ src/components/CountDown/useCountDown.ts
@@ -1,4 +1,4 @@
-import {onMounted, onUnmounted, ref, watch} from "vue";
+import { onMounted, onUnmounted, ref } from "vue";
 
 export const useCountDown = (time: number) => {
   const countdown = ref(time);
@@ -8,6 +8,9 @@ export const useCountDown = (time: number) => {
   const startCountDown = () => {
     intervalId = setInterval(() => {
       countdown.value--;
+      if (countdown.value === 0) {
+        stopCountDown();
+      }
     }, 1000);
   }
 
@@ -18,19 +21,8 @@ export const useCountDown = (time: number) => {
     }
   }
 
-  onMounted(() => {
-    startCountDown();
-
-    watch(countdown, (newVal) => {
-      if (newVal === 0) {
-        stopCountDown();
-      }
-    });
-  });
-
-  onUnmounted(() => {
-    stopCountDown();
-  });
+  onMounted(startCountDown);
+  onUnmounted(stopCountDown);
 
   return {
     countdown,
