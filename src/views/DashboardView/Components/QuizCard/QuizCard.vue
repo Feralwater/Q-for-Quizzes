50644e2d211f40559ffff4208dc14d6a099e6eb9
BuyakovaVA <lineviktoriya@gmail.com>
2023-12-12 20:06:45 +0100

    Update QuizCard and DashboardView components

--- src/views/DashboardView/Components/QuizCard/QuizCard.vue
+++ src/views/DashboardView/Components/QuizCard/QuizCard.vue
@@ -1,18 +1,88 @@
 <script setup lang="ts">
+import router from '@/router';
+import icon from '@/assets/logo.svg';
 
+const startQuiz = () => {
+  router.push({
+    name: 'quiz',
+  });
+};
 </script>
 
 <template>
-  <v-card>
-    <v-card-title>
-      <h1>Dashboard</h1>
+  <v-card
+    style="overflow: initial; z-index: initial"
+    class="mx-auto my-12 card"
+    max-width="374"
+  >
+    <v-img
+      :src="icon"
+      class="card__img"
+    />
+
+    <v-card-item>
+      <v-card-title class="card__text">
+        Vuejs basic
+      </v-card-title>
+
+      <v-card-subtitle class="card__text">
+        <span class="me-1">
+          Test your vuejs knowledge
+        </span>
+
+        <v-icon
+          color="primary"
+          icon="mdi-school"
+          size="small"
+        />
+      </v-card-subtitle>
+    </v-card-item>
+
+    <v-divider class="mx-4 mb-1" />
+
+    <v-card-title class="card__text">
+      You will have:
     </v-card-title>
-    <v-card-text>
-      <p>Dashboard</p>
-    </v-card-text>
+
+    <div class="px-4">
+      <v-chip-group>
+        <v-chip class="card__chip">
+          <v-icon
+            color="primary"
+            icon="mdi-clock-outline"
+            size="small"
+          />
+          <span class="card__chip__text card__text">
+            20 minutes
+          </span>
+        </v-chip>
+
+        <v-chip class="card__chip">
+          <v-icon
+            color="primary"
+            icon="mdi-help-circle-outline"
+            size="small"
+          />
+          <span class="card__chip__text card__text">
+            10 questions
+          </span>
+        </v-chip>
+      </v-chip-group>
+    </div>
+
+    <v-card-actions>
+      <v-btn
+        color="primary"
+        variant="outlined"
+        @click="startQuiz"
+      >
+        Start Quiz
+      </v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
 <style scoped>
-
+@import 'QuizCard.scss';
 </style>
+
--- src/views/DashboardView/DashboardView.vue
+++ src/views/DashboardView/DashboardView.vue
@@ -4,6 +4,55 @@ import QuizCard from '@/views/DashboardView/Components/QuizCard/QuizCard.vue';
 </script>
 
 <template>
+  <div class="my-4 text-subtitle-1">
+    Quiz rules
+    <v-icon
+      color="error"
+      icon="mdi-book-open-page-variant"
+      size="small"
+    />
+  </div>
+
+  <div>
+    <v-icon
+      color="error"
+      icon="mdi-check-circle"
+      size="small"
+    />
+    <span>
+      Time Limit:
+    </span>
+    <span>
+      Each question has a designated time limit within which you must provide an answer. You can submit your answer before the time runs out to move on to the next question.
+    </span>
+  </div>
+  <div>
+    <v-icon
+      color="error"
+      icon="mdi-check-circle"
+      size="small"
+    />
+    <span>
+      No Backtracking:
+    </span>
+    <span>
+      Once you submit an answer, you cannot go back to change it. Be sure of your choice before moving forward.
+    </span>
+  </div>
+  <div>
+    <v-icon
+      color="error"
+      icon="mdi-check-circle"
+      size="small"
+    />
+    <span>
+      Automatic Progression:
+    </span>
+    <span>
+      If the time runs out or you leave the quiz page, you'll be automatically moved to the next question. Any selected choices will be saved, so make every second count!
+    </span>
+  </div>
+
   <v-container>
     <v-row>
       <v-col
