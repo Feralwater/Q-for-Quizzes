571a17aac851d84137048261edb1b3b54e31d4e4
BuyakovaVA <lineviktoriya@gmail.com>
2023-12-18 20:31:38 +0100

    Improve accessibility in RulesButton component
    This update adds an 'aria-label' and 'tabindex' to the RulesButton for better accessibility. Also adds 'aria-hidden' attribute to the icon to successfully hide it from screen readers.

--- src/views/DashboardView/Components/RulesButton/RulesButton.vue
+++ src/views/DashboardView/Components/RulesButton/RulesButton.vue
@@ -4,15 +4,17 @@
     variant="outlined"
     class="infoButton"
     icon
+    aria-label="Open book"
+    tabindex="0"
   >
     <v-icon
       color="lightTextColour"
       icon="mdi-book-open-page-variant"
       size="50"
+      aria-hidden="true"
     />
   </v-btn>
 </template>
-
 <style scoped>
 @import 'RulesButton.scss';
 </style>
