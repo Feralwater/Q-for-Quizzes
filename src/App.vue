c26e09158a6266a14c49a0da37ce3b60ae098b49
BuyakovaVA <lineviktoriya@gmail.com>
2023-12-21 15:55:30 +0100

    install @vercel/speed-insights

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 </script>
 
 <template>
-  <RouterView />
+  <speed-insights>
+    <RouterView />
+  </speed-insights>
 </template>
