1d753dd9f7a6fea2d2c77c5abd82b02e56f90eb7
BuyakovaVA <lineviktoriya@gmail.com>
2023-12-27 23:24:31 +0100

    add conformation functions

--- src/components/ConformationDialog/ConformationDialog.vue
+++ src/components/ConformationDialog/ConformationDialog.vue
@@ -1,28 +1,44 @@
 <script setup lang="ts">
+defineProps({
+  onConfirm: {
+    type: Function,
+    required: true,
+  },
+  dialog: {
+    type: Boolean,
+    required: true,
+    default: false,
+  },
+  deletedItem: {
+    type: String,
+    required: true,
+  },
+});
 
-
+const emit = defineEmits(['update:dialog']);
 
 </script>
 
 <template>
   <div class="text-center">
     <v-dialog
-      v-model="dialog"
+      :model-value="dialog"
       width="auto"
+      @update:model-value="emit('update:dialog', $event)"
     >
       <v-card>
         <v-card-title class="headline">
           Delete Item
         </v-card-title>
         <v-card-text>
-          Are you sure you want to delete this item?
+          Are you sure you want to delete {{ deletedItem }}?
         </v-card-text>
         <v-card-actions>
           <v-spacer />
           <v-btn
             variant="outlined"
             color="primary"
-            @click="dialog = false"
+            @click="emit('update:dialog', false)"
           >
             <v-icon>mdi-close</v-icon>
             <span>Cancel</span>
--- src/views/ProfileView/QuizTable/QuizTable.vue
+++ src/views/ProfileView/QuizTable/QuizTable.vue
@@ -11,11 +11,23 @@ import ConformationDialog from '@/components/ConformationDialog/ConformationDial
 const { getLocalStorage:getCompletedQuiz, setLocalStorage } = useLocalStorage<CompletedQuiz[]>('completedQuiz', []);
 
 const completedQuiz = ref(getCompletedQuiz());
+const dialog = ref(false);
+const selectedQuizId = ref<number | null>(null);
+const deletedItem = ref('');
 
 const onDelete = (id: number) => {
-  const newCompletedQuiz = completedQuiz.value.filter(quiz => quiz.certificateId !== id);
-  setLocalStorage(newCompletedQuiz);
-  completedQuiz.value = newCompletedQuiz;
+  dialog.value = true;
+  selectedQuizId.value = id;
+  deletedItem.value = `your certificate number ${id}`;
+};
+
+const onConfirm = () => {
+  if (selectedQuizId.value) {
+    const newCompletedQuiz = completedQuiz.value.filter(quiz => quiz.certificateId !== selectedQuizId.value);
+    setLocalStorage(newCompletedQuiz);
+    completedQuiz.value = newCompletedQuiz;
+  }
+  dialog.value = false;
 };
 
 const { mdAndDown } = useDisplay();
@@ -71,7 +83,12 @@ const navigateToCertificate = (id: number) => {
       </v-icon>
     </template>
   </v-data-table>
-  <conformation-dialog />
+  <conformation-dialog
+    v-model:dialog="dialog"
+    :on-confirm="onConfirm"
+    :deleted-item="deletedItem"
+    @update:dialog="dialog = $event"
+  />
 </template>
 
 <style scoped>
