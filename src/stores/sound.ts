9ea52699fa8c1d807ecff3f5f2599be61dac08f7
BuyakovaVA <lineviktoriya@gmail.com>
2023-12-28 15:31:42 +0100

    fix eslint errors

--- src/stores/sound.ts
+++ src/stores/sound.ts
@@ -3,13 +3,12 @@ import { ref } from 'vue';
 
 export const useSound = defineStore('sound', () => {
 
-    const isSoundEnabled = ref<boolean>(true);
-    const toggleSound = () => {
-      isSoundEnabled.value = !isSoundEnabled.value;
-    };
-    return {
-      isSoundEnabled,
-      toggleSound,
-    };
-  },
-);
+  const isSoundEnabled = ref<boolean>(true);
+  const toggleSound = () => {
+    isSoundEnabled.value = !isSoundEnabled.value;
+  };
+  return {
+    isSoundEnabled,
+    toggleSound,
+  };
+});
