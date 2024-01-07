305ebc4b9c6d5dd155817082c25c2056dc2baa42
BuyakovaVA <lineviktoriya@gmail.com>
2024-01-07 20:44:43 +0100

    add dashboard test

---
+++ src/views/DashboardView/__tests__/DashboardView.spec.ts
@@ -0,0 +1,40 @@
+import {  shallowMount } from '@vue/test-utils';
+import DashboardView from '@/views/DashboardView/DashboardView.vue';
+import QuizCard from '@/views/DashboardView/Components/QuizCard/QuizCard.vue';
+import { quizzes } from '@/assets/data/quizzes';
+import { describe, it, expect } from 'vitest';
+import { createVuetify } from 'vuetify';
+import DialogWindow from '../../../components/DialogWindow/DialogWindow.vue';
+const vuetify = createVuetify();
+
+describe('DashboardView.vue', () => {
+  const wrapper = shallowMount(DashboardView, {
+    global: {
+      plugins: [vuetify],
+    },
+  });
+  
+  it('renders the correct number of QuizCard components', () => {
+    const quizCards = wrapper.findAllComponents(QuizCard);
+    expect(quizCards.length).toBe(quizzes.length);
+  });
+
+  it('passes the correct props to QuizCard components', () => {
+    const quizCards = wrapper.findAllComponents(QuizCard);
+    quizCards.forEach((quizCard, index) => {
+      expect(quizCard.props()).toEqual({
+        quizIcon: quizzes[index].image,
+        quizTitle: quizzes[index].name,
+        quizDescription: quizzes[index].description,
+        quizTime: quizzes[index].timeToComplete,
+        quizQuestionsAmount: quizzes[index].questionsAmount,
+        quizId: quizzes[index].id,
+      });
+    });
+  });
+
+  it('renders the DialogWindow component', () => {
+    const dialogWindow = wrapper.findComponent(DialogWindow);
+    expect(dialogWindow.exists()).toBe(true);
+  });
+});
