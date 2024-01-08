3616d1abb25277c53b49aa00c6532eacec0df5fa
BuyakovaVA <lineviktoriya@gmail.com>
2024-01-08 22:45:29 +0100

    add react hook quiz

--- src/assets/data/quizzes.ts
+++ src/assets/data/quizzes.ts
@@ -1,10 +1,12 @@
 import vueLogo from '@/assets/images/logos/vueLogo.svg';
 import piniaLogo from '@/assets/images/logos/piniaLogo.svg';
+import hookLogo from '@/assets/images/logos/hookLogo.svg';
 import { basicVueQuestions } from '@/assets/data/basicVueQuestions';
 import { piniaQuestions } from '@/assets/data/piniaQuestions';
 import type { QuizQuestion } from '@/types/QuizQuestion';
 import { intermediateVueQuestions } from '@/assets/data/intermediateVueQuestions';
-import { advancedVueQuestions } from '@/assets/data/advancedVueQuestions'
+import { advancedVueQuestions } from '@/assets/data/advancedVueQuestions';
+import { reactHooksQuestions } from '@/assets/data/reactHooksQuestions';
 
 const calculateQuizTime = (questions: QuizQuestion[]) => questions.reduce((acc, curr) => acc + curr.timeToAnswer, 0);
 
@@ -16,14 +18,6 @@ export const quizzes = [
     image: vueLogo,
     questionsAmount: basicVueQuestions.length,
     timeToComplete: calculateQuizTime(basicVueQuestions),
-  },
-  {
-    id: 'pinia',
-    name: 'Pinia',
-    description: 'Test your knowledge of Pinia',
-    image: piniaLogo,
-    questionsAmount: piniaQuestions.length,
-    timeToComplete: calculateQuizTime(piniaQuestions),
   },
   {
     id: 'vue-intermediate',
@@ -40,5 +34,21 @@ export const quizzes = [
     image: vueLogo,
     questionsAmount: advancedVueQuestions.length,
     timeToComplete: calculateQuizTime(advancedVueQuestions),
+  },
+  {
+    id: 'pinia',
+    name: 'Pinia',
+    description: 'Test your knowledge of Pinia',
+    image: piniaLogo,
+    questionsAmount: piniaQuestions.length,
+    timeToComplete: calculateQuizTime(piniaQuestions),
+  },
+  {
+    id: 'react-hooks',
+    name: 'React hooks',
+    description: 'Test your knowledge of React hooks',
+    image: hookLogo,
+    questionsAmount: reactHooksQuestions.length,
+    timeToComplete: calculateQuizTime(reactHooksQuestions),
   },
 ];
---
+++ src/assets/data/reactHooksQuestions.ts
@@ -0,0 +1,15 @@
+export const reactHooksQuestions = [
+  {
+    id: 1,
+    question: 'What for do we use \'useState\' hook?',
+    answer: [9],
+    options: [
+      'You can create reactive getters using \'createGetter\' function.',
+      'You can create reactive getters using \'computeGetter\' function.',
+      'You cant create reactive getters in Pinia stores.',
+      'You can create reactive getters using \'getters\' property.',
+    ],
+    timeToAnswer: 40,
+    points: 8,
+  },
+];
