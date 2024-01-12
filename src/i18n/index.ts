d3badc13729ac1dddd955107a7b079c1bed89d7e
BuyakovaVA <lineviktoriya@gmail.com>
2024-01-12 13:12:19 +0100

    move messages to json

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,36 +1,15 @@
 import { createI18n } from 'vue-i18n';
 import { plPluralizationRules } from '@/i18n/rules/pluralization';
 import { datetimeFormats  } from '@/i18n/rules/datetime';
-
-const messages = {
-  'en-US': {
-    quizzes: {
-      vueBasics: {
-        title: 'Vue Basics',
-        description: 'Test your knowledge of Vue.js fundamentals',
-      },
-    },
-    questions: '{n} Questions | {n} Question | {n} Questions',
-    startQuiz: 'Start Quiz',
-  },
-  'pl-PL': {
-    quizzes: {
-      vueBasics: {
-        title: 'Podstawy Vue',
-        description: 'Sprawdź swoją wiedzę na temat podstaw Vue.js',
-      },
-    },
-    questions: '{n} pytanie | {n} pytania | {n} pytań',
-    startQuiz: 'Rozpocznij Quiz',
-  },
-};
+import en from '@/i18n/locales/en.json';
+import pl from '@/i18n/locales/pl.json';
 
 export default createI18n({
   locale: 'pl-PL',
   fallbackLocale: 'en-US',
   legacy: false,
   globalInjection: true,
-  messages,
+  messages: { en, pl },
   pluralRules: {
     'pl-PL': plPluralizationRules,
   },
