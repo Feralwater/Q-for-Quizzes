80adcc766bdca50d6078329710205a2fc3a35c7f
BuyakovaVA <lineviktoriya@gmail.com>
2024-01-16 00:36:38 +0100

    add profile table translation

--- src/views/ProfileView/components/QuizTable/headers.ts
+++ src/views/ProfileView/components/QuizTable/headers.ts
@@ -1,31 +1,25 @@
 export const  headers = [
   {
-    title: 'Quiz Name',
     key: 'quizName',
     sortable: true,
   },
   {
-    title: 'Score',
     key: 'score',
     sortable: true,
   },
   {
-    title: 'Date',
     key: 'date',
     sortable: true,
   },
   {
-    title: 'Quiz Taker',
     key: 'quizTaker',
     sortable: false,
   },
   {
-    title: 'Certificate Number',
     key: 'certificateId',
     sortable: true,
   },
   {
-    title: 'Actions',
     key: 'actions',
     sortable: false,
   },
