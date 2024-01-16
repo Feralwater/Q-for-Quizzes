2fb96afed7d5d3398aef94d828f40e3f81af8238
BuyakovaVA <lineviktoriya@gmail.com>
2024-01-16 21:36:38 +0100

    add language switcher to login view

--- src/views/LoginView/LoginView.vue
+++ src/views/LoginView/LoginView.vue
@@ -7,6 +7,7 @@ import router from '@/router';
 import { useAuthStore } from '@/stores/auth';
 import { useDisplay } from 'vuetify';
 import { trans } from '@/i18n/translation';
+import LanguageSwitcher from '@/components/LanguageSwitcher/LanguageSwitcher.vue';
 
 const { login } = useAuthStore();
 
@@ -27,6 +28,9 @@ const { xs } = useDisplay();
     fluid
     fill-height
   >
+    <div class="languageSwitcher">
+      <LanguageSwitcher />
+    </div>
     <v-row>
       <v-col
         cols="12"
