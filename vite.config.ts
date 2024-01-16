338db68eb2c41a6131423ca85c3ff9e62d193e9f
BuyakovaVA <lineviktoriya@gmail.com>
2024-01-16 20:38:06 +0100

    fix console warnings

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,11 @@ import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_OPTIONS_API__: false,
+    __VUE_PROD_DEVTOOLS__: false,
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false,
+  },
   plugins: [
     vue(),
     vueJsx(),
