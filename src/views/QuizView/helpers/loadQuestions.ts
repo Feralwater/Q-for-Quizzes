212e1b653ccf0e33444e7838e0ffe26e5d807511
BuyakovaVA <lineviktoriya@gmail.com>
2024-01-18 21:28:33 +0100

    add advanced questions translate

--- src/views/QuizView/helpers/loadQuestions.ts
+++ src/views/QuizView/helpers/loadQuestions.ts
@@ -1,18 +1,22 @@
 import type { QuizKeys } from '@/types/quizKeys';
 import type { QuizQuestion } from '@/types/QuizQuestion';
+import { useI18n } from 'vue-i18n';
 
 export const loadQuestions = async (quizKey: QuizKeys): Promise<QuizQuestion[]> => {
+  const i18n = useI18n();
+  const locale = i18n.locale.value;
+
   switch (quizKey) {
     case 'vue-basics':
-      return import('@/assets/questions/en-US/basicVueQuestions').then(m => m.basicVueQuestions);
+      return import(`../../../assets/questions/${locale}/basicVueQuestions`).then(m => m.basicVueQuestions);
     case 'pinia':
-      return import('@/assets/questions/en-US/piniaQuestions').then(m => m.piniaQuestions);
+      return import(`../../../assets/questions/${locale}/piniaQuestions`).then(m => m.piniaQuestions);
     case 'vue-intermediate':
-      return import('@/assets/questions/en-US/intermediateVueQuestions').then(m => m.intermediateVueQuestions);
+      return import(`../../../assets/questions/${locale}/intermediateVueQuestions`).then(m => m.intermediateVueQuestions);
     case 'vue-advanced':
-      return import('@/assets/questions/en-US/advancedVueQuestions').then(m => m.advancedVueQuestions);
+      return import(`../../../assets/questions/${locale}/advancedVueQuestions`).then(m => m.advancedVueQuestions);
     case 'react-hooks':
-      return import('@/assets/questions/en-US/reactHooksQuestions').then(m => m.reactHooksQuestions);
+      return import(`../../../assets/questions/${locale}/reactHooksQuestions`).then(m => m.reactHooksQuestions);
     default:
       throw new Error(`Unknown quiz key: ${quizKey}`);
   }
