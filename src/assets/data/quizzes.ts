20d0102d331f6b04788bd04314ecd454ea82e1a2
BuyakovaVA <lineviktoriya@gmail.com>
2024-01-18 19:03:51 +0100

    remove all quizzes loading

--- src/assets/data/quizzes.ts
+++ src/assets/data/quizzes.ts
@@ -1,44 +1,36 @@
 import vueLogo from '@/assets/images/logos/vueLogo.svg';
 import piniaLogo from '@/assets/images/logos/piniaLogo.svg';
 import hookLogo from '@/assets/images/logos/hookLogo.svg';
-import { basicVueQuestions } from '@/assets/data/basicVueQuestions';
-import { piniaQuestions } from '@/assets/data/piniaQuestions';
-import type { QuizQuestion } from '@/types/QuizQuestion';
-import { intermediateVueQuestions } from '@/assets/data/intermediateVueQuestions';
-import { advancedVueQuestions } from '@/assets/data/advancedVueQuestions';
-import { reactHooksQuestions } from '@/assets/data/reactHooksQuestions';
-
-const calculateQuizTime = (questions: QuizQuestion[]) => questions.reduce((acc, curr) => acc + curr.timeToAnswer, 0);
 
 export const quizzes = [
   {
     id: 'vue-basics',
     image: vueLogo,
-    questionsAmount: basicVueQuestions.length,
-    timeToComplete: calculateQuizTime(basicVueQuestions),
+    questionsAmount: 50,
+    timeToComplete: 2745,
   },
   {
     id: 'vue-intermediate',
     image: vueLogo,
-    questionsAmount: intermediateVueQuestions.length,
-    timeToComplete: calculateQuizTime(intermediateVueQuestions),
+    questionsAmount: 11,
+    timeToComplete: 1230,
   },
   {
     id: 'vue-advanced',
     image: vueLogo,
-    questionsAmount: advancedVueQuestions.length,
-    timeToComplete: calculateQuizTime(advancedVueQuestions),
+    questionsAmount: 20,
+    timeToComplete: 1520,
   },
   {
     id: 'pinia',
     image: piniaLogo,
-    questionsAmount: piniaQuestions.length,
-    timeToComplete: calculateQuizTime(piniaQuestions),
+    questionsAmount: 15,
+    timeToComplete: 595,
   },
   {
     id: 'react-hooks',
     image: hookLogo,
-    questionsAmount: reactHooksQuestions.length,
-    timeToComplete: calculateQuizTime(reactHooksQuestions),
+    questionsAmount: 26,
+    timeToComplete: 1925,
   },
 ];
